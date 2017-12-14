15755ebdcbeddff7f96b8a7ccd8b7a2c8101c5a8
xiaoqi <812817883@qq.com>
2017-12-14 23:01:13 +0800

    sample

--- app/src/main/java/com/noober/floatmenu/MainActivity.java
+++ app/src/main/java/com/noober/floatmenu/MainActivity.java
@@ -8,7 +8,9 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.noober.menu.FloatMenu;
 
@@ -18,12 +20,62 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
 	private Point point = new Point();
+	private ListView listView;
+	private Button btn1;
+	private Button btn2;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		ListView listView = findViewById(R.id.listview);
+		listView = findViewById(R.id.listview);
+		btn1 = findViewById(R.id.btn_fun1);
+		btn2 = findViewById(R.id.btn_fun2);
+
+		init1();
+		init2();
+		init3();
+	}
+
+	private void init1(){
+		final FloatMenu floatMenu = new FloatMenu(this, btn1);
+		floatMenu.items("菜单1", "菜单2", "菜单3");
+		floatMenu.setOnItemClickListener(new FloatMenu.OnItemClickListener() {
+			@Override
+			public void onClick(View v, int position) {
+				Toast.makeText(MainActivity.this, "菜单"+position, Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		btn1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				floatMenu.show();
+			}
+		});
+
+	}
+
+	private void init2(){
+		final FloatMenu floatMenu = new FloatMenu(this, btn2);
+		floatMenu.inflate(R.menu.popup_menu);
+		floatMenu.setOnItemClickListener(new FloatMenu.OnItemClickListener() {
+			@Override
+			public void onClick(View v, int position) {
+				Toast.makeText(MainActivity.this, "菜单"+position, Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		btn2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				floatMenu.show();
+			}
+		});
+	}
+
+	private void init3(){
 		List<String> itemList = new ArrayList<>();
 		for (int i = 0; i < 20; i++) {
 			itemList.add("菜单" + i);
